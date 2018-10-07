e7fba6406827af1769d883036329716c0523bcee
Gen Nagano <gen.nagano.cc20120701@gmail.com>
2018-10-07 23:53:28 +0800

    roles ansible_os_init downloaded

---
+++ provision/roles.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+#make current dir same as file dir
+SCRIPT_DIR=`dirname $0`
+
+roles=(
+"gano2018.ansible_os_init"
+#"gano2018.ansible_git"     depended
+#"gano2018.ansible_gitflow"
+#"gano2018.ansible_centos_anyenv"
+#"gano2018.ansible_centos_ruby"
+#"gano2018.ansible_centos_rails"
+)
+
+for role in ${roles[@]}
+do
+  ansible-galaxy install $role --roles-path ${SCRIPT_DIR}/roles/
+done
