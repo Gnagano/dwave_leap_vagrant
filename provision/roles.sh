225b579a374f9dd3b87d67e6018ec3089e5a6ae8
Gen Nagano <gen.nagano.cc20120701@gmail.com>
2018-10-08 10:07:56 +0800

    install roles/ansible_centos_anyenv

--- provision/roles.sh
+++ provision/roles.sh
@@ -7,7 +7,7 @@ roles=(
 "gano2018.ansible_os_init"
 #"gano2018.ansible_git"     depended
 #"gano2018.ansible_gitflow"
-#"gano2018.ansible_centos_anyenv"
+"gano2018.ansible_centos_anyenv"
 #"gano2018.ansible_centos_ruby"
 #"gano2018.ansible_centos_rails"
 )
